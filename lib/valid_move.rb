9f0888d632896f657f07c7b35c680c0c87eea88a
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-29 02:06:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,24 +1,25 @@
-
-def valid_move?(board, position)
- 	spot = position.to_i - 1
- 	if !position_taken?(board, spot) && spot.between?(0,8)
- 		true
-
-  else
-  false
-
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
 
-end
-
-
-def position_taken?(board, position)
-   if board[position] == " " || board[position] == "" || board[position] == nil
-   	false
-
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
     else
-   	true
-
+      return false
     end
+  end
 
-  end 
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
